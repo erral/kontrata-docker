729dadfbc20903c18cf49c0e5704826217ef748b
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-10-15 15:52:57 +0200

    rename indexes

--- create_index.sh
+++ create_index.sh
@@ -1,2 +1,6 @@
-curl -XPUT localhost:9200/contracts
-curl -XPUT localhost:9200/contracts/_settings -H "Content-Type: application/json" -d '{"index.mapping.total_fields.limit": 2000}'
+curl -XPUT localhost:9200/contracts_es
+curl -XPUT localhost:9200/contracts_es/_settings -H "Content-Type: application/json" -d '{"index.mapping.total_fields.limit": 2000}'
+
+curl -XPUT localhost:9200/contracts_eu
+curl -XPUT localhost:9200/contracts_eu/_settings -H "Content-Type: application/json" -d '{"index.mapping.total_fields.limit": 2000}'
+
--- delete_index.sh
+++ delete_index.sh
@@ -1,1 +1,2 @@
-curl -XDELETE localhost:9200/contracts
+curl -XDELETE localhost:9200/contracts_es
+curl -XDELETE localhost:9200/contracts_eu
